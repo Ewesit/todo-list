6cb28bc5111d8bb682d2d07800768208bca6628c
Lowa Ewesit <lowaewesi@gmail.com>
2024-03-14 11:53:07 +0300

    styling the body using tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,12 @@ import './App.css'
 import './index.css'
 import {TodoWrapper} from './components/TodoWrapper';
 
-
-
-
 function App() {
   return(
-    <div className='App bg-gray-800 min-h-screen flex flex-col justify-center items-center'>
+    <div className='App'>
 
       <TodoWrapper>
-        <h1 className="text-6xl font-bold text-white">My TODO List</h1>
+        <h1 className="text-5xl font-bold text-white">My TODO List</h1>
       </TodoWrapper>
       
       
